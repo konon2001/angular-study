de27cb66512ca2486646fc628aa55ec1be7d9797
조우진(Mac) <woojin@tmon.co.kr>
2015-08-25 19:03:14 +0900

    Watch 까지 도전?

--- controllers/AboutController.js
+++ controllers/AboutController.js
@@ -4,5 +4,17 @@
 	.module('angular.study.about', ['angular.study.myDirectives'])
 	.controller('AboutController', function($scope){
 			this.titleStr = "About View....";
+			$scope.testData = "";
+
+			$scope.$watch('testData', function(){
+				var strArr = $scope.testData.split(' ');
+				angular.forEach(strArr, function(val){
+					if(val === "WoW") {
+						alert("WoW");
+					} else {
+						console.log("No Wow!");
+					}
+				});
+			});
 		});
 })();
