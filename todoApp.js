439221f492b201c37de410cbf352a2f95ecf9e9b
조우진(Mac) <woojin@tmon.co.kr>
2015-08-25 16:03:49 +0900

    route 추가

--- todoApp.js
+++ todoApp.js
@@ -1,8 +1,22 @@
 (function(){
 	'use strict';
 
-	angular.module('MyToDo', [ 'ngCookies', 'ngResource', 'ngRoute', 'ToDoCtrls'])
-
-	;
+	angular
+		.module('MyToDo', [ 'ngCookies', 'ngResource', 'ngRoute', 'angular.study.todo', 'angular.study.about'])
+		.config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: './views/todo.html',
+        controller: 'ToDoController'
+      })
+      .when('/about', {
+        templateUrl: './views/about.html',
+        controller: 'AboutController',
+        controllerAs: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
 })();
 
